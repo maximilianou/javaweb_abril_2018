69de1e4c8be9c0747f009ce04ed273db258dc4c5
educacion <educacion@blanca>
2018-04-09 15:42:00 -0300

    Primera clase, viendo html, pero sin html. es decir java y url, porque un alumno pregutno por un bot

---
+++ bot/src/bot/Bot.java
@@ -0,0 +1,36 @@
+package bot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class Bot {
+
+    public static void main(String[] args) {
+        System.out.println("[..] Bot  porque me lo dijo ERLICH");
+
+        try {
+            URL laUrl = new URL("https://theguardian.com");
+            System.out.println(" todo bien traje la pagina");
+            InputStream entradaBinaria = laUrl.openConnection().getInputStream();
+            System.out.println(" tardo en cargar, pero trajo!!");
+            InputStreamReader entradaTexto = new InputStreamReader(entradaBinaria);
+            BufferedReader entrada = new BufferedReader(entradaTexto);
+            System.out.println(entrada.readLine());
+            while (entrada.ready()) {
+                System.out.println(entrada.readLine());
+            }
+        } catch (MalformedURLException ex) {
+            System.out.println("[ERROR] Bot : " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("[ERROR] Bot : " + ex.getMessage());
+        }
+
+        System.out.println("[OK] Bot  porque me lo dijo ERLICH");
+
+    }
+
+}
+
