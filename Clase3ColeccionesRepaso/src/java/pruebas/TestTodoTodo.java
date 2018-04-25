b2df740492b528651f97267dd32922c573ae494a
educacion <educacion@blanca>
2018-04-25 16:38:16 -0300

    venimos ya con listas y conjuntos de elemento unicos

---
+++ Clase3ColeccionesRepaso/src/java/pruebas/TestTodoTodo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+/**
+ *
+ * @author educacion
+ */
+public class TestTodoTodo {
+    public static void main(String[] args) {
+        System.out.println("[..] Test Completo");
+        
+        TestListas.main(args);
+        TestConjuntos.main(args);
+        
+        System.out.println("[OK] Test Completo");
+    }
+}
