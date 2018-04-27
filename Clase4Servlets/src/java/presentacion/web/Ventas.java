ff313763c6d0c014d77148f2e30b6871d9fa64a0
educacion <educacion@blanca>
2018-04-27 09:58:23 -0300

    y consultamos la base, la base esta

--- Clase4Servlets/src/java/presentacion/web/Ventas.java
+++ Clase4Servlets/src/java/presentacion/web/Ventas.java
@@ -2,6 +2,13 @@ package presentacion.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -54,14 +61,42 @@ public class Ventas extends HttpServlet {
             HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-//        processRequest(request, response);
         PrintWriter salida = response.getWriter();
-        
+
         salida.print("Primer Texto para la web");
         salida.print("Primer Texto para la web");
-        // 1. Queremos Acceder a la Base de DAtos
-        // 2. creamos la base de datos
-        
+
+        try {
+            //        processRequest(request, response);
+            // 1. Queremos Acceder a la Base de DAtos
+            // 2. creamos la base de datos
+
+            // 0. el driver de conexion con la Base acual
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            // 1. conectar
+            Connection conectar = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/gestion_ventas_db",
+                    "educacion",
+                    "educacion");
+            salida.print("\n Coneccion OK \n");
+            // 2. sentenncia
+            PreparedStatement sentencia =
+                    conectar.prepareStatement(
+                            " SELECT * FROM ventas ");
+            // 3. resultado mostrar
+            ResultSet resultado = sentencia.executeQuery();
+            while( resultado.next() ){
+            salida.print("\n "+ 
+                    resultado.getString("ven_numero")
+                    +" \n");
+            }
+            // 4. capturar errores
+            // 5. cerrar conexion
+
+        } catch (Exception ex) {
+            salida.print("\n Coneccion ERROR \n" 
+            + ex.getMessage());
+        }
     }
 
     /**
