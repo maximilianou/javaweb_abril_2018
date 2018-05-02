f39f5230630cc10bc0cb4134055d5838fd067791
Maximiliano Usich <maximilianou@gmail.com>
2018-05-02 15:42:00 -0300

    Ordenando por criterio, inicio del proyectos

---
+++ Clase5Colectando/src/java/pruebas/TestOrdenandoPorCriterio.java
@@ -0,0 +1,16 @@
+package pruebas;
+
+/**
+ * <pre>
+ * Referencia, comienzo encarando la programacion
+ * del ejercicio, con TDD  como concepto basico.
+ * https://en.wikipedia.org/wiki/Test-driven_development
+ * </pre>
+ */
+public class TestOrdenandoPorCriterio {
+    public static void main(String[] args) {
+        System.out.println("[..] Por Criterio");
+        
+        System.out.println("[OK] Por Criterio");
+    }
+}
