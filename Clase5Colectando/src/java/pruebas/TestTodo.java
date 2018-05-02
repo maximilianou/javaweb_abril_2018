c334ace7e972c3befd6e09a393558384592a6253
Maximiliano Usich <maximilianou@gmail.com>
2018-05-02 17:00:12 -0300

    Ordenando con un criterio, nombre de producto

---
+++ Clase5Colectando/src/java/pruebas/TestTodo.java
@@ -0,0 +1,13 @@
+package pruebas;
+
+public class TestTodo {
+    public static void main(String[] args) {
+        System.out.println("[..] Testeando Tdooodoo");
+        
+        TestCliente.main(args);
+        
+        TestOrdenandoPorCriterio.main(args);
+        
+        System.out.println("[OK] Testeando Tdooodoo");
+    }
+}
